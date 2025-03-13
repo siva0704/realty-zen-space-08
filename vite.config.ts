61001d04785be528251e5682d3da7424afec9aef
AmullyaPatil <patilamullya@gmail.com>
2025-03-13 21:10:01 +0530

    project deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Nestora/", // Add this for GitHub Pages
+  base: "/realty-zen-space-08/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // Fix alias for "@/components/..."
